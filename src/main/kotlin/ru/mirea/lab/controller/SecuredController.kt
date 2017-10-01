f08cac576ccfe6754b4b4db06d21ae408cb9bb6a
Cancer <mizantrop2397@gmail.com>
2017-10-02 02:50:25 +0300

    Removed jks

--- src/main/kotlin/ru/mirea/lab/controller/SecuredController.kt
+++ src/main/kotlin/ru/mirea/lab/controller/SecuredController.kt
@@ -1,17 +1,11 @@
 package ru.mirea.lab.controller
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import ru.mirea.lab.service.SecuredService
-
 
 @Controller
 class SecuredController {
-    @Autowired
-    lateinit var service: SecuredService
-
     @GetMapping("/")
     fun greeting(model: Model) = with(model) {
         "index"
--- src/main/kotlin/ru/mirea/lab/service/SecuredService.kt
+++
@@ -1,8 +0,0 @@
-package ru.mirea.lab.service
-
-import org.springframework.stereotype.Service
-
-@Service
-class SecuredService {
-    fun request(requestData: String) = requestData
-}
